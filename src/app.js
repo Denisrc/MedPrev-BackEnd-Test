309169411b8aa0c035d67363373e24ebefbe881b
João Denis <joao.denisr@gmail.com>
2020-07-30 13:43:46 -0300

    create person schema

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const Koa = require('koa');
-
+const mongoose = require('mongoose');
 const koaBody = require('koa-body');
 
 const app = new Koa();
+
+const config = require('./config').development;
+
+mongoose.connect(config.DBHost, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(koaBody());
 
